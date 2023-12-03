96f599543ee11f4441e217032d0450eef796ae97
david prorok <davidjamesprorok@gmail.com>
2023-12-03 13:59:27 -0600

    Updated sandbox

--- apps/fe/src/app/pages/sandbox/sandbox.tsx
+++ apps/fe/src/app/pages/sandbox/sandbox.tsx
@@ -7,6 +7,7 @@ export function Sandbox(props: SandboxProps) {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Sandbox!</h1>
+      <p>I hope you have fun!</p>
     </div>
   );
 }
